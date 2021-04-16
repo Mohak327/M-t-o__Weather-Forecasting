4cdc31e469325f300e733b32a3aac5186440bcd6
Mohak Sharma <mohak327@gmail.com>
2021-04-16 14:09:37 +0530

    added index.html in /docs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('weather-app'));
 
